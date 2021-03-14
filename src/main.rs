5ae705671ee992033db4320e05bddd25c0bdfafa
Mihyaeru <mihyaeru21@gmail.com>
2021-03-14 12:44:51 +0900

    :sparkles: convert/nonconvert

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::fmt;
 use std::os::raw::c_int;
 use std::ptr;
-use winapi::shared::minwindef::{LPARAM, LRESULT, WPARAM};
+use winapi::shared::minwindef::{LPARAM, LRESULT, WORD, WPARAM};
 use winapi::shared::windef::HHOOK;
 use winapi::um::libloaderapi::GetModuleHandleW;
 use winapi::um::winuser::{
@@ -10,7 +10,9 @@ use winapi::um::winuser::{
     LLKHF_UP, WH_KEYBOARD_LL,
 };
 
-static mut h_hook: HHOOK = ptr::null_mut();
+static mut H_HOOK: HHOOK = ptr::null_mut();
+static mut LAST_EVENT: Option<KeyEvent> = None;
+static mut SENDING_SAME_KEY: bool = false;
 
 struct KeyEvent {
     raw: KBDLLHOOKSTRUCT,
@@ -64,26 +66,93 @@ impl fmt::Debug for KeyEvent {
     }
 }
 
+unsafe fn create_input(code: WORD, is_up: bool) -> INPUT {
+    let mut input = INPUT::default();
+    input.type_ = INPUT_KEYBOARD;
+    let mut ki = input.u.ki_mut();
+    ki.wVk = code;
+    ki.dwFlags = if is_up { KEYEVENTF_KEYUP } else { 0 };
+    input
+}
+
+unsafe fn send_inputs(mut inputs: Vec<INPUT>) {
+    SendInput(
+        inputs.len() as u32,
+        inputs.as_mut_ptr(),
+        std::mem::size_of::<INPUT>() as c_int,
+    );
+    // TODO: check result
+}
+
 #[no_mangle]
 pub unsafe extern "system" fn handler(code: c_int, wp: WPARAM, lp: LPARAM) -> LRESULT {
     if let Some(mut k) = ptr::NonNull::new(lp as *mut KBDLLHOOKSTRUCT) {
         let event = KeyEvent::new(*k.as_mut());
-        if !event.is_injected() && event.vk_code() == 0x38 {
-            let mut input = INPUT::default();
-            input.type_ = INPUT_KEYBOARD;
-            let mut ki = input.u.ki_mut();
-            ki.wVk = 0x39;
-            ki.dwFlags = if event.is_up() { KEYEVENTF_KEYUP } else { 0 };
 
-            SendInput(1, &mut input, std::mem::size_of::<INPUT>() as c_int);
-            return -1;
+        if !event.is_injected() {
+            match event.vk_code() {
+                0xa4 => {
+                    let inputs = vec![create_input(0xa4, event.is_up())];
+                    send_inputs(inputs);
+                    return -1;
+                }
+                0xa5 => {
+                    let inputs = vec![create_input(0xa5, event.is_up())];
+                    send_inputs(inputs);
+                    return -1;
+                }
+                _ => {}
+            }
         }
-        println!("debug: {:?}", event);
+
+        if let Some(last) = &LAST_EVENT {
+            if !SENDING_SAME_KEY && event.is_up() && event.vk_code() == last.vk_code() {
+                match event.vk_code() {
+                    // left alt
+                    0xa4 => {
+                        let inputs = vec![
+                            create_input(0x07, false),
+                            create_input(0x07, true),
+                            create_input(0xa4, true),
+                            create_input(0x1d, false),
+                            create_input(0x1d, true),
+                        ];
+                        SENDING_SAME_KEY = true;
+                        send_inputs(inputs);
+                        SENDING_SAME_KEY = false;
+                        return -1;
+                    }
+                    // right alt
+                    0xa5 => {
+                        let inputs = vec![
+                            create_input(0x07, false),
+                            create_input(0x07, true),
+                            create_input(0xa5, true),
+                            create_input(0x1c, false),
+                            create_input(0x1c, true),
+                        ];
+                        SENDING_SAME_KEY = true;
+                        send_inputs(inputs);
+                        SENDING_SAME_KEY = false;
+                        return -1;
+                    }
+                    _ => {}
+                }
+            }
+        }
+
+        let thread_id = std::thread::current().id();
+        println!(
+            "debug({:?}): hoge: {:?}, {:?}",
+            thread_id, SENDING_SAME_KEY, event
+        );
+
+        LAST_EVENT = Some(event);
     }
 
     // 握りつぶすなら -1 を返す
 
-    CallNextHookEx(h_hook, code, wp, lp)
+    CallNextHookEx(H_HOOK, code, wp, lp)
 }
 
 fn main() {
@@ -91,8 +160,8 @@ fn main() {
 
     unsafe {
         let instance = GetModuleHandleW(ptr::null());
-        h_hook = SetWindowsHookExW(WH_KEYBOARD_LL, Some(handler), instance, 0);
-        if h_hook == ptr::null_mut() {
+        H_HOOK = SetWindowsHookExW(WH_KEYBOARD_LL, Some(handler), instance, 0);
+        if H_HOOK == ptr::null_mut() {
             panic!("failed to set hook!");
         }
     }
